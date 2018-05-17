dc96c6b20a65f503211752158425573a20fdd8c3
hexfaker <6dec3fb8@gmail.com>
2018-05-17 22:54:07 +0800

    Added basic logging to STDOUT

--- pycar/test/test_logger.py
+++ pycar/test/test_logger.py
@@ -5,9 +5,17 @@
 
 # well there is actually no use of this file.
 # import ../logger.py
+import logging
+
 
 def test():
-    pass
+    logging.warning("Test warning")
+    logging.info("Test info")
+    logging.debug("Test debug info")
+    logging.error("Test error")
+    # logging.exception("Test exception")
+    logging.critical("Test critical")
+
 
 if __name__ == '__main__':
     test()
