18858262c479babb87c25a58d97383bc78b0d1c4
Hexfaker <6dec3fb8@gmail.com>
2018-05-23 19:30:50 +0800

    Fixed import relationship

--- main.py
+++ main.py
@@ -4,12 +4,20 @@
 # main entry
 
 # import pycar
-from pycar import misc
+from pycar import misc, motor
+
+import time
 
 def main():
+    md = motor.MotorDifferentor(12, 16, 18, 11)
+    md.set_speed(30, 0)
+    time.sleep(1)
+    md.set_speed(0, 0)
+
     print("Dummy")
 
 
 if __name__ == '__main__':
+    misc.init()
     main()
 
